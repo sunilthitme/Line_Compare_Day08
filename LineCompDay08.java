2d3477d548dbf3ae540930a9615c8e415778269c
sunilthitme <thitamesunil808@gmail.com>
2021-12-14 22:21:58 +0530

    Creating object

--- LineCompDay08.java
+++ LineCompDay08.java
@@ -21,19 +21,27 @@ public class LineCompDay08 {
 				int p2 = s.nextInt();
 				System.out.println("Enter y co-ordinate of forth point: ");
 				int q2 = s.nextInt();
-				s.close();
+				Calculate call=new Calculate();
+				call.length(x1, x2, y1, y2, p1, p2, q1, q2);
+				
+			}
+}
+class Calculate{
+	public static void length(int x1,int x2,int y1, int y2,int p1,int p2,int q1, int q2) {
+		
 
 				Double length_of_line1 = Math.sqrt((x2-x1)*(x2-x1)+(y2-y1)*(y2-y1));
 				Double length_of_line2 = Math.sqrt((p2-p1)*(p2-p1)+(q2-q1)*(q2-q1));
 				
 				System.out.printf("length of 1st end points are : %.2f %n", length_of_line1);
 				System.out.printf("length of 2nd end points are : %.2f %n", length_of_line2);
-				boolean ans = length_of_line1.equals(length_of_line2);
-				if(ans)
+				int ans = length_of_line1.compareTo(length_of_line2);
+				if(ans==0)
 					System.out.println("Both lines are equal.");
 				else
 					System.out.println("Both lines are not equal.");
 			}
-		}
+}	
+
 
 
